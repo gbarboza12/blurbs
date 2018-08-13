dacc211ff1809a99c89544d83ac4ef0574262b1a
Guadalupe <gbarboza1231@gmail.com>
2018-08-12 20:54:13 -0500

    added missing stuff to server file

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 require('rootpath')();
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
+require("dotenv").config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require( 'morgan');
 const cors = require('cors');
 const jwt = require('./helpers/jwt');
 const errorHandler = require('./helpers/errorhandler');
+const path = require("path")
 
 const app = express();
 
 const API_PORT = process.env.API_PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,5 +27,9 @@ app.use(require('./routes/api/users'));
 app.use(require('./routes/api/queueentries'));
 
 app.use(errorHandler);
+
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
