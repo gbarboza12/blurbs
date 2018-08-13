55a1d8a17012788b041126188915d839e5bd1832
Guadalupe <gbarboza1231@gmail.com>
2018-08-12 20:14:00 -0500

    attempt to fix syntax error that came up on heroku

--- start.js
+++ start.js
@@ -2,8 +2,6 @@
 require('babel-register')({
     presets: [ 'env' ]
 })
-require("babel-core/register");
-require("babel-polyfill");
 
 // Import the rest of our application.
 module.exports = require('./server.js')
